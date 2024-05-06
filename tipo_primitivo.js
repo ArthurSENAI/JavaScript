999448bd216b92054e33f4bb33369bba01ac66e2
ArthurSENAI <arthur.s.araujo7@aluno.senai.br>
2024-05-06 15:37:53 -0300

    Create tipo_primitivo.js

---
+++ tipo_primitivo.js
@@ -0,0 +1,24 @@
+/*
+TIPOS PRIMITIVOS
+    String
+    Number
+    Boolean
+    Object - Null
+    Undefine
+*/
+
+var nome = 'Arthur';
+var numero = 1234;
+var letra  = "A";
+var decimal = 1234.1234;
+var logico = true;
+var vnull = null;
+var indefinida;
+
+console.log(typeof nome, nome);
+console.log(typeof numero, numero);
+console.log(typeof letra, letra);
+console.log(typeof decimal, decimal);
+console.log(typeof logico, logico);
+console.log(typeof vnull, vnull);
+console.log(typeof indefinida, indefinida);
