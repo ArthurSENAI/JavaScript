c679c7bec0bd5dc47f0386786dc2527545e43990
ArthurSENAI <arthur.s.araujo7@aluno.senai.br>
2024-05-06 14:53:12 -0300

    Update variavel.js

--- variavel.js
+++ variavel.js
@@ -1,3 +1,56 @@
-const n3 = 'Constante';
-var n1;
-let n2;
+const n3 = 'Constante';//
+let nome2;//
+var nome1;//
+
+/*
+*****
+VAR - VARIAVEL GLOBAL
+Aceita SOBREESCRITA, Aceita RE-DECLARAÇÃO, CHAMAR EM QUALQUER LUGAR DO CODIGO
+*****
+var n1 = 'Arthur';
+if(true){
+    var n1 = 'Araujo'
+};   
+var n1 = 'Santos';
+console.log(n1);
+*/
+
+/*
+*****
+LET - VARIAVEL DE ESCOPO
+Aceita SOBREESCRITA, NÃO Aceita RE-DECLARAÇÃO, Só visivel dentro do seu bloco de codigo, 
+Aceita vizualizar variaveis globais  
+*****
+let n2 = 'Arthur';
+if(true){
+   console.log(n2)
+};   
+
+if(true){
+    let n2 = 'Arthur'
+    console.log(n2)
+};   
+
+let n2 = 'Arthur';
+if(true){
+    n2 = 'Araujo'
+   console.log(n2)
+}; 
+*/
+
+/*
+*****
+CONST -  VARIAVEL DE ESCOPO
+NÃO Aceita SOBREESCRITA, NÃO Aceita RE-DECLARAÇÃO, Deve ser inicializada com conteudo,
+*****
+const n3 = 'Arthur';
+if(true){
+   console.log(n3)
+};   
+
+if(true){
+    const n3 = 'Arthur'
+    console.log(n3)
+};   
+*/
+
