9f6a0b53b8b50d159501e98b14beb69cb6114af3
ArthurSENAI <arthur.s.araujo7@aluno.senai.br>
2024-05-06 16:49:09 -0300

    Update 004-tipo_number.js

--- 004-tipo_number.js
+++ 004-tipo_number.js
@@ -27,3 +27,18 @@ var number = Number(false);
 console.log(typeof number, number);
 var number = Number(true);
 console.log(typeof number, number);
+
+// Variavel Date
+var number = new Date();
+console.log(typeof number, number);
+
+// Variavel  Date em Number
+var number = Number(new Date());
+console.log(typeof number, number);
+
+// Converter de string para numero usando o parseInt
+
+var n = '29.230421598321684'
+console.log(typeof n, n)
+console.log(typeof parseInt(n), parseInt(n))
+
