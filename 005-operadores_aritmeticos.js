cd9e2d5aeb8ad4197754a8587a17fc1a2f58bcb4
ArthurSENAI <arthur.s.araujo7@aluno.senai.br>
2024-05-06 17:17:00 -0300

    Create 005-operadores_aritmeticos.js

---
+++ 005-operadores_aritmeticos.js
@@ -0,0 +1,19 @@
+/*
+TIPOS
+    Adição(+)
+    Subtração(-)
+    Divisão(/)
+    Multiplicação(*)
+    Resto da Divisão(%)
+    Incremento(++)
+    Descremento(--)
+    Expoente(**)
+*/
+
+let v1 = 2;
+let v2 = 3;
+let soma = v1+v2;
+console.log("A soma é:"+soma)
+console.log("A soma é:",v1+v2)
+console.log("A soma é:"+parseInt(v1+v2))
+console.log("A soma é:"+Number(v1+v2))
