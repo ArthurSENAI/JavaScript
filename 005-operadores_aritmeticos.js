c5015ab8bb7a12f9da2dcf939a4aeebeeafa0f16
ArthurSENAI <arthur.s.araujo7@aluno.senai.br>
2024-05-06 17:30:15 -0300

    Update 005-operadores_aritmeticos.js

--- 005-operadores_aritmeticos.js
+++ 005-operadores_aritmeticos.js
@@ -14,6 +14,7 @@ TIPOS
 let v1 = 10;
 let v2 = 15;
 let soma = v1+v2;
+console.log(v1+v2)
 console.log("A soma é: "+soma)
 console.log("A soma é:",v1+v2)
 console.log("A soma é: "+parseInt(v1+v2))
@@ -23,6 +24,7 @@ console.log("A soma é: "+Number(v1+v2))
 let v3 = 10;
 let v4 = 5;
 let subtracao = v3-v4;
+console.log(v3-v4)
 console.log("A subtração é: "+subtracao)
 console.log("A subtração é:",v3-v4)
 console.log("A subtração é: "+parseInt(v3-v4))
@@ -32,6 +34,7 @@ console.log("A subtração é: "+Number(v3-v4))
 let v5 = 10;
 let v6 = 5;
 let divisao = v5/v6;
+console.log(v5/v6)
 console.log("A divisão é: "+divisao)
 console.log("A divisão é:",v5/v6)
 console.log("A divisão é: "+parseInt(v5/v6))
@@ -41,6 +44,7 @@ console.log("A divisão é: "+Number(v5/v6))
 let v7 = 10;
 let v8 = 5;
 let multipicacao = v7*v8;
+console.log(v7*v8)
 console.log("A multiplicação é: "+multipicacao)
 console.log("A multiplicação é:",v7*v8)
 console.log("A multiplicação é: "+parseInt(v7*v8))
