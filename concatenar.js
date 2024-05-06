6dbc645ed258f291d2c9e865a57c4260e12e84ff
ArthurSENAI <arthur.s.araujo7@aluno.senai.br>
2024-05-06 15:46:14 -0300

    Update concatenar.js

--- concatenar.js
+++ concatenar.js
@@ -2,8 +2,19 @@ var dono = 'Arthur';
 var ddd = '(61)';
 var telefone = '9 9999-9999';
 var numero = ddd.concat(" " + telefone);
-console.log ("O Cliente", dono, "tem o seguinte numero de celular",ddd, telefone);
-console.log ("O Cliente "+ dono + " tem o seguinte numero de celular "+ numero);
 
-let texto = `O Cliente ${dono} tem o seguinte numero de celular ${ddd} ${telefone}`
-console.log(texto)
+// Concatenado com ','
+console.log ("O Cliente", dono, "tem o seguinte numero de celular",ddd, telefone);
+// Concatenado com '+'
+console.log ("O Cliente "+ dono + " tem o seguinte numero de celular "+ numero);
+// Concatenado em variavel com '$'
+let texto = `O Cliente ${dono} tem o seguinte numero de celular ${ddd} ${telefone}`;
+console.log(texto);
+
+//Idade como Numero
+var idade = 28;
+console.log(idade + 1)
+
+//Transforma idade em String
+var idade = 28;
+console.log(idade.toString() + 1);
