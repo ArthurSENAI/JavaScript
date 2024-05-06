13843c8cf1292341e90e2908465f8fb57727af3e
ArthurSENAI <arthur.s.araujo7@aluno.senai.br>
2024-05-06 17:23:08 -0300

    Update 005-operadores_aritmeticos.js

--- 005-operadores_aritmeticos.js
+++ 005-operadores_aritmeticos.js
@@ -10,6 +10,7 @@ TIPOS
     Expoente(**)
 */
 
+// Adição
 let v1 = 2;
 let v2 = 3;
 let soma = v1+v2;
@@ -17,3 +18,12 @@ console.log("A soma é:"+soma)
 console.log("A soma é:",v1+v2)
 console.log("A soma é:"+parseInt(v1+v2))
 console.log("A soma é:"+Number(v1+v2))
+
+// Subtração
+let v3 = 10;
+let v4 = 5;
+let subtracao = v3-v4;
+console.log("A subtração é:"+subtracao)
+console.log("A subtração é:",v3-v4)
+console.log("A subtração é:"+parseInt(v3-v4))
+console.log("A subtração é:"+Number(v3-v4))
