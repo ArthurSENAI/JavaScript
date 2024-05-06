b2bba6e84aa4fb0d938742c680366a412c3f69a8
ArthurSENAI <arthur.s.araujo7@aluno.senai.br>
2024-05-06 16:35:53 -0300

    Update 004-tipo_number.js

--- 004-tipo_number.js
+++ 004-tipo_number.js
@@ -3,7 +3,27 @@
         parseInt
         parseFloat
 */
-
-let idade = 29;
+// Variavel Number
+var idade = 29;
+console.log(typeof idade, idade);
 console.log(typeof idade, idade+1);
 
+// Variavel String
+var idade = '29';
+console.log(typeof idade, idade);
+console.log(typeof idade, idade+1);
+
+// Variavel String em Number
+var idade = Number('29');
+console.log(typeof idade, idade);
+console.log(typeof idade, idade+1);
+
+// Variavel Boolean
+var number = true;
+console.log(typeof number, number);
+
+// Variavel Boolean em Number
+var number = Number(false);
+console.log(typeof number, number);
+var number = Number(true);
+console.log(typeof number, number);
