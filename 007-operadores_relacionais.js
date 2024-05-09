ca4467df14ccc0e3d759bc489024320e81e82bc2
ArthurSENAI <arthur.s.araujo7@aluno.senai.br>
2024-05-09 16:53:59 -0300

    Update 007-operadores_relacionais.js

--- 007-operadores_relacionais.js
+++ 007-operadores_relacionais.js
@@ -11,41 +11,60 @@
 //else: SENÃO
 
 
+// Declara as variáveis a e b com os valores 5 e 7, respectivamente
 let a = 5;
 let b = 7;
 
+// Verifica se a é igual a b
 if (a == b) {
+    // Se sim, imprime a mensagem "a é igual a b"
     console.log("a é igual a b");
 } else {
+    // Se não, imprime a mensagem "a não é igual a b"
     console.log("a não é igual a b");
 }
 
-if (a != b) {
+// Verifica se a é diferente de b
+if (a!= b) {
+    // Se sim, imprime a mensagem "a é diferente de b"
     console.log("a é diferente de b");
 } else {
+    // Se não, imprime a mensagem "a não é diferente de b"
     console.log("a não é diferente de b");
 }
 
+// Verifica se a é menor que b
 if (a < b) {
+    // Se sim, imprime a mensagem "a é menor que b"
     console.log("a é menor que b");
 } else {
+    // Se não, imprime a mensagem "a não é menor que b"
     console.log("a não é menor que b");
 }
 
+// Verifica se a é menor ou igual a b
 if (a <= b) {
+    // Se sim, imprime a mensagem "a é menor ou igual a b"
     console.log("a é menor ou igual a b");
 } else {
+    // Se não, imprime a mensagem "a não é menor ou igual a b"
     console.log("a não é menor ou igual a b");
 }
 
+// Verifica se a é maior que b
 if (a > b) {
+    // Se sim, imprime a mensagem "a é maior que b"
     console.log("a é maior que b");
 } else {
+    // Se não, imprime a mensagem "a não é maior que b"
     console.log("a não é maior que b");
 }
 
+// Verifica se a é maior ou igual a b
 if (a >= b) {
+    // Se sim, imprime a mensagem "a é maior ou igual a b"
     console.log("a é maior ou igual a b");
 } else {
+    // Se não, imprime a mensagem "a não é maior ou igual a b"
     console.log("a não é maior ou igual a b");
 }
