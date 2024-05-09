6b6b709e049fbbe64e2e3901cc84a338ad27184f
ArthurSENAI <arthur.s.araujo7@aluno.senai.br>
2024-05-09 16:54:03 -0300

    Update 010-laço_repetição(for).js

--- 010-laço_repetição(for).js
+++ 010-laço_repetição(for).js
@@ -7,31 +7,48 @@ de código um número específico de vezes.
   ou executar uma ação repetida com base em uma 
   condição. */
 
-
+// Este é um loop for que será executado 6 vezes, com a variável i assumindo os valores de 0 a 5
 for (let i = 0; i <= 5; i++) {
+    // Este irá registrar o valor atual de i no console
     console.log(i)
 }
 
-const fruntas = ['maça', 'banana', 'laranja', 'pera', 'melancia'];
+// Este é um array de strings representando diferentes tipos de frutas
+const frutas = ['maça', 'banana', 'laranja', 'pera', 'melancia'];
 
-for (let i = 0; i <= fruntas.length; i++) {
-    console.log('Eu gosto de ' + fruntas[i])
+// Este é um loop for que será executado uma vez para cada elemento no array de frutas
+for (let i = 0; i <= frutas.length; i++) {
+    // Este irá registrar uma mensagem no console indicando que você gosta da fruta atual
+    // A fruta atual é acessada usando a notação de colchetes com a variável i como índice
+    console.log('Eu gosto de ' + frutas[i]+'\n')
 }
 
+// Este é um objeto com três propriedades: firstName, lastName e age
 const person = {
     firstName: 'Seu',
     lastName: 'Ze',
     age: 20
 };
 
+// Este é um loop for...in que irá iterar sobre cada propriedade no objeto person
 for (let key in person) {
+    // Este irá registrar o nome da propriedade atual e seu valor no console
     console.log(key + ': ' + person[key])
 };
 
+// Este é um loop for que será executado 10 vezes, com a variável i assumindo os valores de 0 a 9
 for (let i = 0; i < 10; i++) {
+    // Este irá verificar se o valor atual de i é igual a 5
     if (i === 5) {
+        // Se for, este irá interromper o loop e impedir que ele continue executando
         break;
     }
+    // Se o valor atual de i não for igual a 5, este irá registrar o valor atual de i no console
     console.log(i);
 }
 
+// Este é um loop for que será executado 6 vezes, com a variável i assumindo os valores de 0 a 5
+for (let i = 0; i <= 5; i++) {
+    // Este irá registrar o valor atual de i no console
+    console.log(i)
+}
